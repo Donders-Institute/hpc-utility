21fdb65b978a229903e47aa0f3d59fa554ad857f
Hong Lee <h.lee@donders.ru.nl>
2019-02-28 18:07:50 +0100

    initial implementation of node meminfo

--- internal/cmd/cluster.go
+++ internal/cmd/cluster.go
@@ -1,6 +1,17 @@
 package cmd
 
 import (
+	"encoding/csv"
+	pxml "encoding/xml"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
+	"strconv"
+	"strings"
+
 	trqhelper "github.com/Donders-Institute/hpc-torque-helper/pkg/client"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -8,17 +19,31 @@ import (
 
 var xml bool
 
+const (
+	gib int64 = 1024 * 1024 * 1024
+)
+
 // variable may be set at the build time to fix the default location for the TorqueHelper server certificate.
 var defTorqueHelperCert string
+
+// Ganglia URLs for retriving node resource information
+var gangliaAccessNodeMeminfoURL = "http://ganglia.dccn.nl/rawdata.php?c=Mentat%20Cluster&cols[]=mem_total.VAL&cols[]=mem_free.VAL&noheader"
+var gangliaAccessNodeDiskinfoURL = "http://ganglia.dccn.nl/rawdata.php?c=Mentat%20Cluster&cols[]=disk_total.VAL&cols[]=disk_free.VAL&noheader"
+
+var gangliaComputeNodeMeminfoURL = "http://ganglia.dccn.nl/rawdata.php?c=Torque%20Cluster&cols[]=mem_total.VAL&cols[]=mem_free.VAL&noheader"
+var gangliaComputeNodeDiskinfoURL = "http://ganglia.dccn.nl/rawdata.php?c=Torque%20Cluster&cols[]=disk_total.VAL&cols[]=disk_free.VAL&noheader"
 
 func init() {
 
 	qstatCmd.Flags().BoolVarP(&xml, "xml", "x", false, "XML output")
 
-	clusterCmd.AddCommand(qstatCmd, configCmd, jobTraceCmd, jobMeminfoCmd)
 	clusterCmd.PersistentFlags().StringVarP(&TorqueServerHost, "server", "s", "torque.dccn.nl", "Torque server hostname")
 	clusterCmd.PersistentFlags().IntVarP(&TorqueHelperPort, "port", "p", 60209, "Torque helper service port")
 	clusterCmd.PersistentFlags().StringVarP(&TorqueHelperCert, "cert", "c", defTorqueHelperCert, "Torque helper service certificate")
+
+	nodeCmd.AddCommand(nodeMeminfoCmd)
+	jobCmd.AddCommand(jobTraceCmd, jobMeminfoCmd)
+	clusterCmd.AddCommand(qstatCmd, configCmd, jobCmd, nodeCmd)
 
 	rootCmd.AddCommand(clusterCmd)
 }
@@ -69,6 +94,13 @@ var configCmd = &cobra.Command{
 	},
 }
 
+// job related subcommands
+var jobCmd = &cobra.Command{
+	Use:   "job",
+	Short: "Retrieve information about a cluster job.",
+	Long:  ``,
+}
+
 var jobTraceCmd = &cobra.Command{
 	Use:   "jobtrace [JobID]",
 	Short: "Print job's trace log available on the Torque server.",
@@ -101,4 +133,126 @@ var jobMeminfoCmd = &cobra.Command{
 			log.Errorf("fail get job memory utilisation: %+v\n", err)
 		}
 	},
+}
+
+// node related subcommands
+var nodeCmd = &cobra.Command{
+	Use:   "node",
+	Short: "Retrieve information about cluster nodes.",
+	Long:  ``,
+}
+
+var nodeMeminfoCmd = &cobra.Command{
+	Use:        "memfree",
+	Short:      "Print total and free memory of the cluster's access nodes.",
+	Long:       ``,
+	ValidArgs:  []string{"access", "compute"},
+	ArgAliases: []string{"access", "a", "compute", "c"},
+	Run: func(cmd *cobra.Command, args []string) {
+
+		urls := map[string]string{
+			"access":  gangliaAccessNodeMeminfoURL,
+			"compute": gangliaComputeNodeMeminfoURL,
+		}
+
+		for _, n := range args {
+
+			url, err := url.Parse(urls[n])
+			if err != nil {
+				log.Errorf("invalid URL: %s\n", url)
+				continue
+			}
+
+			// make HTTP GET call to the ganglia endpoint.
+			c := &http.Client{}
+			req, _ := http.NewRequest("GET", url.String(), nil)
+			resp, err := c.Do(req)
+			if err != nil {
+				log.Errorf("%s", err)
+				continue
+			}
+			if resp.StatusCode != 200 {
+				log.Errorf("fail get ganglia data: %s (HTTP CODE %d)", url.String(), resp.StatusCode)
+				continue
+			}
+
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			// the tabular data is enclosed by <pre></pre> tag in the returned HTML.
+			data := gangliaRawHTML{}
+			if err := pxml.Unmarshal(body, &data); err != nil {
+				log.Errorf("fail get ganglia data: %s\n", err)
+				continue
+			}
+
+			log.Debugf("ganglia tabular data: %s\n", data.Pre)
+
+			// parse the tabular data
+			r := csv.NewReader(strings.NewReader(data.Pre))
+			r.Comma = '\t'
+			r.TrailingComma = true
+			r.TrimLeadingSpace = true
+			r.LazyQuotes = true
+			for {
+				o := gangliaResourceInfo{}
+				err := parseGangliaRawdata(r, &o)
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Debugf("%+v\n", err)
+					continue
+				}
+				log.Infoln(o.String())
+			}
+		}
+	},
+}
+
+type gangliaRawHTML struct {
+	XMLName pxml.Name `xml:"html"`
+	Pre     string    `xml:"body>pre"`
+}
+
+type gangliaResourceInfo struct {
+	Host  string
+	Total int64
+	Free  int64
+}
+
+func (g *gangliaResourceInfo) String() string {
+	return fmt.Sprintf("%20s %4d %4d\n", g.Host, g.Free*1024/gib, g.Total*1024/gib)
+}
+
+func parseGangliaRawdata(reader *csv.Reader, v interface{}) error {
+	record, err := reader.Read()
+	if err != nil {
+		return err
+	}
+
+	s := reflect.ValueOf(v).Elem()
+	if len(record) < s.NumField() {
+		return fmt.Errorf(fmt.Sprintf("expect %d field, got %d: %s", s.NumField(), len(record), record))
+	}
+
+	// reduce record to the expected amount of fields.
+	record = record[0:s.NumField()]
+
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		switch f.Type().String() {
+		case "string":
+			f.SetString(record[i])
+		case "int64":
+			ival, err := strconv.ParseInt(record[i], 10, 0)
+			if err != nil {
+				return err
+			}
+			f.SetInt(ival)
+		default:
+			return fmt.Errorf("unsupported type: %s", f.Type().String())
+		}
+	}
+	return nil
 }
