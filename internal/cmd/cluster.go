a08000811038c7b11d12e4c95dd17323c7ebc1b6
Hong Lee <h.lee@donders.ru.nl>
2019-03-20 09:45:59 +0100

    try automerging vnc sessions by username

--- internal/cmd/cluster.go
+++ internal/cmd/cluster.go
@@ -378,6 +378,7 @@ var nodeVncCmd = &cobra.Command{
 		// tabular display
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{"Username", "VNC session"})
+		table.SetAutoMergeCells(true)
 		for _, h := range _hosts {
 			vncs := _vncs[h]
 			sort.Slice(vncs, func(i, j int) bool {
