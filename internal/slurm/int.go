2fed1c7b10ce6e250f62e671242148a2631a5ff4
Hurng-Chun Lee <h.lee@donders.ru.nl>
2024-12-06 15:04:26 +0100

    used the Gres attribute to parse network bandwidth and tmpdir size

--- internal/slurm/int.go
+++ internal/slurm/int.go
@@ -20,23 +20,24 @@ import (
 // ```
 // NodeName=dccn-c084 Arch=x86_64 CoresPerSocket=32
 //
-//	CPUAlloc=0 CPUEfctv=63 CPUTot=64 CPULoad=0.01
-//	AvailableFeatures=(null)
-//	ActiveFeatures=(null)
-//	Gres=cpu:amd:1,gpu:nvidia_a100-sxm4-40gb:4(S:0-1)
-//	NodeAddr=dccn-c084 NodeHostName=dccn-c084 Version=22.05.10
-//	OS=Linux 4.18.0-553.8.1.el8_10.x86_64 #1 SMP Tue Jul 2 07:26:33 EDT 2024
-//	RealMemory=515578 AllocMem=0 FreeMem=375161 Sockets=2 Boards=1
-//	CoreSpecCount=1 CPUSpecList=63 MemSpecLimit=4096
-//	State=IDLE ThreadsPerCore=1 TmpDisk=3604221 Weight=1 Owner=N/A MCS_label=N/A
-//	Partitions=gpu,batch
-//	BootTime=2024-11-19T13:53:34 SlurmdStartTime=2024-11-19T13:54:41
-//	LastBusyTime=2024-11-20T15:16:28
-//	CfgTRES=cpu=63,mem=515578M,billing=63,gres/gpu=4,gres/gpu:nvidia_a100-sxm4-40gb=4
-//	AllocTRES=
-//	CapWatts=n/a
-//	CurrentWatts=0 AveWatts=0
-//	ExtSensorsJoules=n/s ExtSensorsWatts=0 ExtSensorsTemp=n/s
+//		CPUAlloc=0 CPUEfctv=63 CPUTot=64 CPULoad=0.01
+//		AvailableFeatures=(null)
+//		ActiveFeatures=(null)
+//		Gres=cpu:amd:1,gpu:nvidia_a100-sxm4-40gb:4(S:0-1),tmp:3500G,network:10G
+//	    GresUsed=cpu:amd:0,gpu:nvidia_a100-sxm4-40gb:4(IDX:0-3),license:0,tmp:1073741824000,network:0
+//		NodeAddr=dccn-c084 NodeHostName=dccn-c084 Version=22.05.10
+//		OS=Linux 4.18.0-553.8.1.el8_10.x86_64 #1 SMP Tue Jul 2 07:26:33 EDT 2024
+//		RealMemory=515578 AllocMem=0 FreeMem=375161 Sockets=2 Boards=1
+//		CoreSpecCount=1 CPUSpecList=63 MemSpecLimit=4096
+//		State=IDLE ThreadsPerCore=1 TmpDisk=3604221 Weight=1 Owner=N/A MCS_label=N/A
+//		Partitions=gpu,batch
+//		BootTime=2024-11-19T13:53:34 SlurmdStartTime=2024-11-19T13:54:41
+//		LastBusyTime=2024-11-20T15:16:28
+//		CfgTRES=cpu=63,mem=515578M,billing=63,gres/gpu=4,gres/gpu:nvidia_a100-sxm4-40gb=4
+//		AllocTRES=
+//		CapWatts=n/a
+//		CurrentWatts=0 AveWatts=0
+//		ExtSensorsJoules=n/s ExtSensorsWatts=0 ExtSensorsTemp=n/s
 //
 // ```
 func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
@@ -46,6 +47,7 @@ func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
 	cpuAllocated := 0
 	memAllocated := 0
 	gpuAllocated := 0
+	tmpAllocated := 0
 
 	var err error
 
@@ -86,9 +88,11 @@ func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
 
 			case "Gres":
 				reCpu := regexp.MustCompile(`cpu:(amd|intel):[0-9]+`)
+				reNet := regexp.MustCompile(`network:([0-9]+)G`)
+				reTmp := regexp.MustCompile(`tmp:([0-9]+)G`)
 
+				// CPU manufacturer
 				cpuinfo := reCpu.FindStringSubmatch(keyValue[1])
-
 				if len(cpuinfo) < 2 {
 					return info, fmt.Errorf("unexpected CPU information: %s", keyValue[1])
 				}
@@ -99,6 +103,44 @@ func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
 				case "intel":
 					info.IsIntel = true
 					info.IsAMD = false
+				}
+
+				// network bandwidth
+				netinfo := reNet.FindStringSubmatch(keyValue[1])
+				if len(netinfo) < 2 {
+					return info, fmt.Errorf("unexpected network information: %s", keyValue[1])
+				}
+				netGb, err := strconv.Atoi(netinfo[1])
+				if err != nil {
+					log.Errorf("invalid number of network bandwidth GB: %s", err)
+					return info, err
+				}
+				info.NetworkGbps = netGb
+
+				// tmp disk size
+				tmpinfo := reTmp.FindStringSubmatch(keyValue[1])
+				if len(tmpinfo) < 2 {
+					return info, fmt.Errorf("unexpected tmpdir size information: %s", keyValue[1])
+				}
+				tmpGb, err := strconv.Atoi(tmpinfo[1])
+				if err != nil {
+					log.Errorf("invalid number of tmpdir size GB: %s", err)
+					return info, err
+				}
+				info.TotalDiskGB = tmpGb
+
+			case "GresUsed":
+
+				// used tmp disk size
+				reTmp := regexp.MustCompile(`tmp:([0-9]+)G`)
+				tmpinfo := reTmp.FindStringSubmatch(keyValue[1])
+				if len(tmpinfo) < 2 {
+					return info, fmt.Errorf("unexpected tmpdir usage information: %s", keyValue[1])
+				}
+				tmpAllocated, err = strconv.Atoi(tmpinfo[1])
+				if err != nil {
+					log.Errorf("invalid number of tmpdir usage GB: %s", err)
+					return info, err
 				}
 
 			case "CfgTRES":
@@ -124,12 +166,15 @@ func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
 				}
 
 			case "TmpDisk":
-				diskMB, err := strconv.Atoi(keyValue[1])
-				if err != nil {
-					log.Errorf("invalid number of tmp disk size: %s", err)
-					return info, err
+				// get total tmpdir size if it is not resolved from Gres
+				if info.TotalDiskGB == 0 {
+					diskMB, err := strconv.Atoi(keyValue[1])
+					if err != nil {
+						log.Errorf("invalid number of tmp disk size: %s", err)
+						return info, err
+					}
+					info.TotalDiskGB = diskMB / 1024
 				}
-				info.TotalDiskGB = diskMB / 1024
 
 			default:
 				// do nothing
@@ -144,17 +189,7 @@ func parseSingleNodeInfo(out string) (trqhelper.NodeResourceStatus, error) {
 	info.AvailProcs = info.TotalProcs - cpuAllocated
 	info.AvailGPUS = info.TotalGPUS - gpuAllocated
 	info.AvailMemGB = info.TotalMemGB - (memAllocated / 1024)
-
-	// TODO: in Slurm, the TmpDisk is the total node scratch space
-	//       it doesn't tell how much has left; and no quota limit
-	//       at all.
-	//       This should be improved further with the GRES approach.
-	info.AvailDiskGB = info.TotalDiskGB
-
-	// TODO: this should be done dynamically.  Nevertheless, it
-	//       is anyway a static value given that we run purely
-	//       the 10 Gbs network within the Slurm cluster.
-	info.NetworkGbps = 10
+	info.AvailDiskGB = info.TotalDiskGB - tmpAllocated
 
 	return info, nil
 
@@ -188,7 +223,7 @@ func parseMultipleNodeInfo(out string) []trqhelper.NodeResourceStatus {
 // get information of all Slurm nodes.
 func GetNodeInfo(id string) ([]trqhelper.NodeResourceStatus, error) {
 
-	args := []string{"show", "node"}
+	args := []string{"show", "node", "--detail"}
 
 	if id != "" && id != "ALL" {
 		args = append(args, id)
